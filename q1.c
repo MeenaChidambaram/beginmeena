578d7a6a1ea443fa17c3358ec1249fcebc7ac12e
Muthumeenal C <meenuct.95@gmail.com>
2016-07-13 11:32:23 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("its a even number");
+}
+else
+{
+printf("its a odd number");
+}
+getch();
+return 0;
+}
